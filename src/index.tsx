c9d570fb89668d999804178b03671cc0d5c0c5e2
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-06 01:05:00 -0800

    Added Checkbox, Label, Select, Card

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,7 @@
 export * from "./lib/icons";
 export * from "./components/button";
 export * from "./components/accordion/accordion";
+export * from "./components/checkbox/checkbox";
+export * from "./components/input/input";
+export * from "./components/label/label";
+export * from "./components/select";
